5ecfbd7edbbba21f2cb449f4a78aa231409eece0
Ramesh Ragala <rameshragala@gmail.com>
2020-09-04 22:12:37 +0530

    Add files via upload

---
+++ Lab/WordCountProgram2/WordCountMapper.java
@@ -0,0 +1,26 @@
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.*;
+import java.util.StringTokenizer;
+public class WordCountMapper extends Mapper<LongWritable,Text,Text,IntWritable> {
+	private static final IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+	
+	public void map(LongWritable key, Text value, Context context) throws 
+	IOException, InterruptedException
+	{
+		String line = value.toString();
+		StringTokenizer itr = new StringTokenizer(line);
+		while(itr.hasMoreTokens())
+		{
+			word.set(itr.nextToken());
+			if(word.charAt(0)>=65 && word.charAt(0)<=90)
+			{
+			context.write(word,one);
+			}
+		}
+	}
+
+}
